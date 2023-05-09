46552a02751e0dd4971c83bfb1827f6e6f4c045d
Peter <vagnerpetel2020@gmail.com>
2023-05-09 15:12:18 -0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -5,19 +5,138 @@ import { projects } from "./projectsArray";
 // components
 import Project from "./components/Project";
 import Landing from "./components/Landing";
+import Navbar from "./components/Navbar";
 
 console.log(projects);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <Landing />
-      <div className="project-container">
+      <header className="App-header">
+        <Navbar />
+      </header>
+      <section className="landing">
+        <Landing />
+      </section>
+
+      <section id="projects" className="project-section">
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          version="1.1"
+          class="static-svg"
+          // transform="rotate(180)"
+        >
+          <path
+            d="
+        M0 67,
+        C 373, 283 
+          1022, -140
+          1920, 306
+        V 600
+        H 0
+        V 67  
+        Z"
+          ></path>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          version="1.1"
+          class="static-svg"
+          transform="rotate(180)"
+        >
+          <path
+            d="
+        M0 67,
+        C 373, 283 
+          1022, -140
+          1920, 306
+        V 600
+        H 0
+        V 67  
+        Z"
+          ></path>
+        </svg>
+
         {projects.map((project, index) => (
           <Project key={index} project={project} />
         ))}
-      </div>
+      </section>
+      <section id="contact">
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          version="1.1"
+          class="static-svg"
+          // transform="rotate(180)"
+        >
+          <path
+            d="
+        M0 67,
+        C 373, 283 
+          1022, -140
+          1920, 306
+        V 600
+        H 0
+        V 67  
+        Z"
+          ></path>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          version="1.1"
+          class="static-svg"
+          transform="rotate(180)"
+        >
+          <path
+            d="
+        M0 67,
+        C 373, 283 
+          1022, -140
+          1920, 306
+        V 600
+        H 0
+        V 67  
+        Z"
+          ></path>
+        </svg>
+      </section>
+      <section id="about">
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          version="1.1"
+          class="static-svg"
+          // transform="rotate(180)"
+        >
+          <path
+            d="
+        M0 67,
+        C 373, 283 
+          1022, -140
+          1920, 306
+        V 600
+        H 0
+        V 67  
+        Z"
+          ></path>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/200/svg"
+          version="1.1"
+          class="static-svg"
+          transform="rotate(180)"
+        >
+          <path
+            d="
+        M0 67,
+        C 373, 283 
+          1022, -140
+          1920, 306
+        V 600
+        H 0
+        V 67  
+        Z"
+          ></path>
+        </svg>
+      </section>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="nav-bar">
+      <a href="#projects">Projects</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+    </div>
+  );
+};
+
+export default Navbar;
