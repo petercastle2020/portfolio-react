4a10e4a0c3bdb64c993fc715f9394bc4806f947e
Peter <vagnerpetel2020@gmail.com>
2023-05-09 17:40:11 -0300

    responsive layout for project components.

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,8 +9,8 @@ const Project = ({ project }) => {
     <div className="project">
       <h2 className="project-title">{project.title}</h2>
       <div className="project-contents">
-        <div>
-          <img src="#" alt="project-illustration" />
+        <div className="project-img">
+          <img src={project.image} alt="project-illustration" />
         </div>
         <div className="project-about">
           <p>{project.aboutText}</p>
--- src/projectsArray.js
+++ src/projectsArray.js
@@ -1,9 +1,17 @@
+const binanceImage =
+  "https://res.cloudinary.com/dqjwxv8ck/image/upload/v1682026205/g6vjzwrdqhzrzohia5c4.webp";
+const disneyImage =
+  "https://res.cloudinary.com/dqjwxv8ck/image/upload/v1682372330/zhzhargcokmb3unqbh9o.webp";
+const xboxoneImage =
+  "https://res.cloudinary.com/dqjwxv8ck/image/upload/v1682026217/eptov0p9fliuzkxiftkj.webp";
+
 // use this words to ref icons {react, node.js, mongodb, express.js, github, javascript, css, html}
 const projects = [
   {
     title: "Project 1",
-    image: "#",
-    aboutText: "This is project 1.",
+    image: disneyImage,
+    aboutText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales tortor urna, eget maximus magna porttitor nec. Proin nunc purus, finibus vel magna at, semper tincidunt mauris. Aenean eu velit enim. Morbi et lorem ligula. Proin ac eros vel sem bibendum placerat. Class aptent taciti sociosqu ad litora torquent per.",
     skills: [
       "React",
       "Node.js",
@@ -17,9 +25,17 @@ const projects = [
   },
   {
     title: "Project 2",
-    image: "#",
-    aboutText: "This is project 2.",
+    image: binanceImage,
+    aboutText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales tortor urna, eget maximus magna porttitor nec. Proin nunc purus, finibus vel magna at, semper tincidunt mauris. Aenean eu velit enim. Morbi et lorem ligula. Proin ac eros vel sem bibendum placerat. Class aptent taciti sociosqu ad litora torquent per.",
     skills: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "Project 3",
+    image: xboxoneImage,
+    aboutText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales tortor urna, eget maximus magna porttitor nec. Proin nunc purus, finibus vel magna at, semper tincidunt mauris. Aenean eu velit enim. Morbi et lorem ligula. Proin ac eros vel sem bibendum placerat. Class aptent taciti sociosqu ad litora torquent per.",
+    skills: ["github", "javascript", "css", "html"],
   },
 ];
 
