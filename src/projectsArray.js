9f70e02db92ecd9012adf4c43b4a283292aab274
Peter <vagnerpetel2020@gmail.com>
2023-06-19 13:43:07 -0300

    add translation to the project sections.

--- src/projectsArray.js
+++ src/projectsArray.js
@@ -1,18 +1,15 @@
-const binanceImage =
-  "https://res.cloudinary.com/dqjwxv8ck/image/upload/v1682026205/g6vjzwrdqhzrzohia5c4.webp";
-const disneyImage =
-  "https://res.cloudinary.com/dqjwxv8ck/image/upload/v1682372330/zhzhargcokmb3unqbh9o.webp";
-const xboxoneImage =
-  "https://res.cloudinary.com/dqjwxv8ck/image/upload/v1682026217/eptov0p9fliuzkxiftkj.webp";
+const younewsIMG = "./images/younews-responsive.jpg";
+const memesApiIMG = "./images/meme-api-cover.jpg";
+const twitterExtIMG = "./images/hidden128-2x.png";
 
 // use this words to ref icons {react, node.js, mongodb, express.js, github, javascript, css, html}
 const projects = [
   {
-    title: "You News",
-    image: disneyImage,
+    title: "YouNews",
+    image: younewsIMG,
     aboutText: {
-      en: "here would be the EN texat",
-      pt: "BR BR BR BR BR BR BR BR",
+      en: "YouNews is a web application where users can post and read articles on various topics. It provides a platform for news and other related content. It offers a user-friendly interface, search functionality, and the ability to write your own articles.",
+      pt: "YouNews é um aplicativo web onde os usuários podem publicar e ler artigos sobre diversos tópicos. Oferece uma plataforma para notícias e outros conteúdos relacionados. O aplicativo possui uma interface amigável, funcionalidade de busca e a capacidade de publicar seus próprios artigos.",
     },
     gitHubLink: "githublink.something",
     liveDemoLink: "livedemo.something.",
@@ -28,22 +25,30 @@ const projects = [
     ],
   },
   {
-    title: "Project 2",
-    image: binanceImage,
+    title: "Programming memes API",
+    image: memesApiIMG,
     aboutText: {
-      en: "here would be the EN texat",
-      pt: "BR BR BR BR BR BR BR BR",
+      en: "The Programming Memes API is a fun and engaging tool that provides a collection of programming memes for entertainment and light-hearted humor. Developers and users can fetch random memes, and search for specific memes.",
+      pt: "A API de Memes de Programação é uma ferramenta divertida que oferece uma coleção de memes de programação para entretenimento e diversão. Desenvolvedores e usuários podem buscar memes aleatórios, ou pesquisar por memes específicos.",
     },
     gitHubLink: "githublink.something",
     liveDemoLink: "livedemo.something.",
-    skills: ["HTML", "CSS", "JavaScript"],
+    skills: [
+      "Node.js",
+      "mongodb",
+      "express.js",
+      "github",
+      "JavaScript",
+      "HTML",
+      "CSS",
+    ],
   },
   {
-    title: "Project 3",
-    image: xboxoneImage,
+    title: "Twitter hide it",
+    image: twitterExtIMG,
     aboutText: {
-      en: "here would be the EN texat",
-      pt: "BR BR BR BR BR BR BR BR",
+      en: "Twitter Hide-it is a browser extension that allows users to hide tweets containing certain keywords or phrases on Twitter. Additionally, it also has a function to hide the trending section on the platform.",
+      pt: "O Twitter Hide-it é uma extensão para navegador que permite aos usuários ocultarem tweets que contenham determinadas palavras-chave ou frases no Twitter. Além disso, também possui uma função para ocultar a seção de 'Trending' na plataforma.",
     },
     gitHubLink: "githublink.something",
     liveDemoLink: "livedemo.something.",
